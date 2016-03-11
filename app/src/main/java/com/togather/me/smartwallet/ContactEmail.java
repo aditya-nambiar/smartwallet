f02f2a07d69d688ae6dfc8bfb408acbba3302dc1
sidutta <siddhartha_dutta@live.com>
2016-03-11 21:19:51 +0530

    added some list features

---
+++ app/src/main/java/com/togather/me/smartwallet/ContactEmail.java
@@ -0,0 +1,11 @@
+package com.togather.me.smartwallet;
+
+public class ContactEmail {
+	public String address;
+	public String type;
+
+	public ContactEmail(String address, String type) {
+		this.address = address;
+		this.type = type;
+	}
+}
---
+++ app/src/main/java/com/togather/me/smartwallet/ContactPhone.java
@@ -0,0 +1,11 @@
+package com.togather.me.smartwallet;
+
+public class ContactPhone {
+	public String number;
+	public String type;
+
+	public ContactPhone(String number, String type) {
+		this.number = number;
+		this.type = type;
+	}
+}
