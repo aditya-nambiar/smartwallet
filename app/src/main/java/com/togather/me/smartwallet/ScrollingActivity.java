5c19bfbc0cf3d4d5d43881565dcc62c7d8b2a3c4
sidutta <siddhartha_dutta@live.com>
2016-03-12 10:51:29 +0530

    added bt permeission

--- app/src/main/java/com/togather/me/smartwallet/ScrollingActivity.java
+++ app/src/main/java/com/togather/me/smartwallet/ScrollingActivity.java
@@ -18,8 +18,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-
-
 public class ScrollingActivity extends AppCompatActivity {
 
     private RecyclerView busRidesView;
